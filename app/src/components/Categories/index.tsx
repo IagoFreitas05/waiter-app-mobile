c651719662309aeffa498f18be0f32ab89a3758f
iagofreitas05 <iagofreitas05@gmail.com>
2023-06-21 16:38:56 +0100

    - finished category component.

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -2,7 +2,16 @@ import {categories} from "../../mocks/categories"
 import {Category, Icon} from "./styles";
 import {Text} from "../Text";
 import {FlatList} from "react-native"
-export function Categories(){
+import {useState} from "react";
+
+export function Categories() {
+    const [selectedCategory, setSelectedCategory] = useState<String>("");
+
+    function handleSelectCategory(categoryId: string) {
+        const category = selectedCategory === categoryId ? "" : categoryId;
+        setSelectedCategory(category);
+    }
+
     return (
         <FlatList
             showsHorizontalScrollIndicator={false}
@@ -10,13 +19,17 @@ export function Categories(){
             data={categories}
             keyExtractor={category => category._id}
             contentContainerStyle={{paddingRight: 24}}
-            renderItem={({item: category}) => (
-            <Category>
-                <Icon>
-                    <Text>{category.icon}</Text>
-                </Icon>
-                <Text size={14} weight={600}>{category.name}</Text>
-            </Category>
-        )} />
+            renderItem={({item: category}) => {
+                const isSelected = selectedCategory === category._id;
+                return (
+                    <Category onPress={() => handleSelectCategory(category._id)}>
+                        <Icon>
+                            <Text opacity={isSelected ? 1: 0.5}>{category.icon}</Text>
+                        </Icon>
+                        <Text opacity={isSelected ? 1: 0.5} size={14} weight={600}>{category.name}</Text>
+                    </Category>
+                )
+            }
+            }/>
     );
 }
